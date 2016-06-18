9e6effbd1f40996420188351abda43f5be8cd575
kenan <253877118@qq.com>
2016-06-18 20:00:46 +0800

    frist
    第一版demo

---
+++ assets/cases/01_graphics/01_sprite/lalala.js
@@ -0,0 +1,30 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        // foo: {
+        //    default: null,
+        //    url: cc.Texture2D,  // optional, default is typeof default
+        //    serializable: true, // optional, default is true
+        //    visible: true,      // optional, default is true
+        //    displayName: 'Foo', // optional
+        //    readonly: false,    // optional, default is false
+        // },
+        // ...
+
+        speed:0,
+    },
+
+    // use this for initialization
+    onLoad: function () {
+
+    },
+
+    setFlashAction: function(){
+
+    },
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
